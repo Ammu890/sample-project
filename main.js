07ed3c8e03372f3f7ffeb685c554c7f2b3de8cf2
Amrutha <amruthaammuz0811@gmail.com>
2024-11-11 20:34:24 +0530

    check the comment

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ console.log(sub(589, 245));
 // git status - to check current status
 // git add - to select all files to be commited
 // git commit -m "message" - to save the file to git
+// git log
+// git commit id
+// git checkout master
