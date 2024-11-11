0e730b8a05d918a2579ae18cf12d70fab728bba4
Amrutha <amruthaammuz0811@gmail.com>
2024-11-11 20:07:51 +0530

    Add the numbers

---
+++ index.js
@@ -0,0 +1,5 @@
+const sum = (fNum, sNum) => {
+    return fNum + sNum
+};
+
+module.exports = sum;
---
+++ main.js
@@ -0,0 +1,7 @@
+const sum = require('./index');
+
+console.log(sum(255, 466));
+
+// git status - to check current status
+// git add - to select all files to be commited
+// git commit -m "message" - to save the file to git
